aa8e05dec0682ff2d8b1929d84287dd30cca2bd3
wleha1 <wleha1234@gmail.com>
2024-01-16 15:29:31 +0300

    project3

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,2 +1,8 @@
 #!/usr/bin/env node
+import getName from "../src/cli.js";
+
 console.log('Welcome to the Brain Games!');
+
+getName()
+
+
---
+++ src/cli.js
@@ -0,0 +1,7 @@
+import readlineSync from 'readline-sync';
+
+export default function getName() {
+const name = readlineSync.question('May I have your name? ');
+console.log(`Hello! ${name}`)
+
+}
