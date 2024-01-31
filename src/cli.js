8b2bf203135b69633bc8fb85f9f20fc4aab933c7
wleha1 <klerens1221@gmail.com>
2024-01-31 15:41:45 +0300

    project7

--- src/cli.js
+++ src/cli.js
@@ -2,5 +2,5 @@ import readlineSync from 'readline-sync';
 
 export default function getName() {
   const name = readlineSync.question('May I have your name? ');
-  console.log(`Hello! ${name}`);
+  console.log(`Hello, ${name}`);
 }
